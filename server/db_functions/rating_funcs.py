ed7bdb18cb5153e6313f1618c55cba1500a9e03c
psihoshlem <yungync@gmail.com>
2023-10-22 00:12:18 +0300

    complete merge

--- server/db_functions/rating_funcs.py
+++ server/db_functions/rating_funcs.py
@@ -1,5 +1,9 @@
 from db_functions import db
-from db_functions.recipes_functions import get_short_recipe_by_id
+from db_functions.recipes_functions import (
+    get_short_recipe_by_id,
+    get_prepaired_data
+)
+from db_functions.ml_funcs import add_data_to_dump
 
 collection = db.ratings
 
@@ -9,11 +13,13 @@ def add_rating(login:str, stars:int, recipe_id: str):
         "recipe_id": recipe_id,
         "stars": stars
     })
+    data = get_prepaired_data(recipe_id)
+    add_data_to_dump(login, data, stars)
+
 
 def get_eaten(login:str):
     result = collection.find({"login": login})
     eaten = []
     for item in result:
         eaten.append(get_short_recipe_by_id(str(item["_id"])))
-
     return eaten
