cea80dd78a463f8c3d16ebebff5381f4fa45586e
psihoshlem <yungync@gmail.com>
2023-10-21 15:50:55 +0300

    поиск, параметры поиска

--- server/routers/recipes_router.py
+++ server/routers/recipes_router.py
@@ -4,18 +4,37 @@ from fastapi.responses import FileResponse
 
 from db_functions.recipes_functions import (
     get_recipes_from_db,
-    get_recipe_by_id
+    get_recipe_by_id,
+    get_recipes_by_query
 )
 
-class QuestionsInfo(BaseModel):
-    questions_num: int
+class QuerySchema(BaseModel):
+    query: str
+    desirable_ingredients: list[str]
+    excluded_ingredients: list[str]
 
 router = APIRouter()
+
+
 
 @router.get("/pics/{filename}")
 async def get_image(filename: str):
     image_path = f"./imgs/{filename}"
     return FileResponse(image_path, media_type="image/jpeg")
+
+@router.post("/recipes", tags=["recipes"])
+async def get_recipes_with_query(query: QuerySchema):
+    query_list = [
+        {"ingredients.name": item.capitalize()+" "} for item in query.desirable_ingredients
+    ]
+    query_list.append(
+        {"ingredients.name": {"$nin": query.excluded_ingredients}}
+    )
+    query_list.append(
+        {"description": {"$regex": query.query, "$options": "i"}}
+    )
+    recipes = get_recipes_by_query(query_list)
+    return recipes
 
 @router.get("/recipes", tags=["recipes"])
 async def get_recipes():
